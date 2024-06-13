ae0abac19eb473b1674a12ae127299b31085f861
AhmedMahmoud952 <ahmedmahmoud107107@gmail.com>
2024-06-13 21:42:18 +0300

    Add files via upload

---
+++ 01-Python/01-Introduction/findFour.py
@@ -0,0 +1,2 @@
+mylist = [1,4,6,7,4]
+print(mylist.count(4))
---
+++ 01-Python/01-Introduction/vowels.py
@@ -0,0 +1,3 @@
+vowels = ["a","e","i","o","u"]
+char  = input("Enter a character: ")
+print("It' a vowel") if char.lower() in vowels else print("It's not a vowel")
